ac18abc3577c93565ddb0d548a463eedae57cf68
Axliex <isalikhwan165@gmail.com>
2024-05-29 17:56:47 +0700

    imagepicker test

--- lib/screens/profile/component/profile_avatar.dart
+++ lib/screens/profile/component/profile_avatar.dart
@@ -1,6 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class ProfileAvatar extends StatelessWidget {
+class ProfileAvatar extends StatefulWidget {
+  @override
+  _ProfileAvatarState createState() => _ProfileAvatarState();
+}
+
+class _ProfileAvatarState extends State<ProfileAvatar> {
+  File? _image;
+
+  Future<void> _pickImage() async {
+    try {
+      final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+      if (pickedFile != null) {
+        setState(() {
+          _image = File(pickedFile.path);
+        });
+      } else {
+        print("No image selected.");
+      }
+    } catch (e) {
+      print("Error picking image: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return CircleAvatar(
@@ -10,9 +35,18 @@ class ProfileAvatar extends StatelessWidget {
         children: [
           Align(
             alignment: Alignment.center,
-            child: Text(
+            child: _image == null
+                ? Text(
               'Foto',
               style: TextStyle(color: Colors.deepPurple, fontSize: 20),
+            )
+                : ClipOval(
+              child: Image.file(
+                _image!,
+                fit: BoxFit.cover,
+                width: 180,
+                height: 180,
+              ),
             ),
           ),
           Align(
@@ -20,10 +54,11 @@ class ProfileAvatar extends StatelessWidget {
             child: CircleAvatar(
               radius: 30,
               backgroundColor: Colors.white,
-              child: Icon(
-                Icons.edit,
+              child: IconButton(
+                icon: Icon(Icons.edit),
                 color: Colors.deepPurple,
-                size: 20,
+                iconSize: 20,
+                onPressed: _pickImage,
               ),
             ),
           ),
